bd44755216a26ec3d650af4250441d945c99e3c8
Dylan K <drkunkel7@gmail.com>
2022-02-24 16:39:07 -0600

    a lil bit of css work

--- script.js
+++ script.js
@@ -12,18 +12,8 @@ genButton.addEventListener("click", function() {
     const lowNum = parseInt(document.querySelector("#low-num").value);
     const highNum = parseInt(document.querySelector("#high-num").value);
     const newNum = Math.floor(Math.random() * (highNum - lowNum + 1) + lowNum);
-    
-    // let newNum = Math.random()
-    // console.log(newNum)
-    // const range = highNum - lowNum + 1
-    // console.log(range)
-    // newNum = newNum * range
-    // console.log(newNum)
-    // // console.log(lowNum + " is lowNum")
-    // newNum = newNum + lowNum
-    // console.log(newNum)
-    // newNum = Math.floor(newNum)
-    // console.log(newNum)
-    numContainer.innerHTML = newNum;
-    // console.log(newNum + ` max:${highNum} min: ${lowNum}`)
+
+    if (!newNum) {
+        return;
+    } else numContainer.innerHTML = newNum;
 } )
